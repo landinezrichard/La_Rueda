1909e279235d3e0d8c250ef7dccfc9d73ab370bb
Ricardo <landinezrichard@gmail.com>
2016-03-10 14:09:30 -0500

    Listos estilos AsideMenu

--- dev/app.js
+++ dev/app.js
@@ -1,10 +1,12 @@
 (function () {
 	const showMenu = require('./components/MainMenu');
+	const tabs = require('./components/Tabs');
 	
 
 	document.addEventListener('DOMContentLoaded', onDOMload);
 
 	function onDOMload() {		
 		showMenu.init();
+		tabs.init();
 	}
 }())
